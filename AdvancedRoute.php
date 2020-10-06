87f1f038d2ea584471b501f280e28f525e6ad989
Aghu <andrefpgm@gmail.com>
2020-10-06 17:42:22 -0300

    Ajustes para BASE_URL acceder desde virtual host; AdvancedRoute ordenado

--- AdvancedRoute.php
+++ AdvancedRoute.php
@@ -6,71 +6,29 @@ require_once 'RouterClass.php';
 
 // CONSTANTES PARA RUTEO
 define( "BASE_URL", 'http://' . $_SERVER["SERVER_NAME"] . ':' . $_SERVER["SERVER_PORT"] . 
-                    dirname( $_SERVER["PHP_SELF"] ) . '/' );
+                    (dirname( $_SERVER["PHP_SELF"] ) == '/' ? 
+                        '/' :  
+                        dirname( $_SERVER["PHP_SELF"] ) . '/' ) 
+      );
+// Control en dirname para evitar doble "/" en BASE_URL. 
+// ie: "misitio.com//"
 
 $r = new Router();
 
 // rutas
 
 // learnit.com/
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/
+// http://localhost/web-2/TPE-WEB-2-%20Repo/
 // Nuestra "home"
 $r->addRoute("", "GET","PublicController", "subjects" );
 
-
-
 // learnit.com/courses/
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/courses/
+// http://localhost/web-2/TPE-WEB-2-%20Repo/courses/
 $r->addRoute( "courses", "GET", "PublicController", "courses" );
 
 // learnit.com/courses/:ID
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/courses/2
-
+// http://localhost/web-2/TPE-WEB-2-%20Repo/courses/2
 $r->addRoute( "courses/:ID", "GET", "PublicController", "courseDetail" );
-
-
-
-// learnit.com/admin
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin
-$r->addRoute( "admin", "GET", "AdminController", "admin" );
-
-// learnit.com/admin/courses/add
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/add
-$r->addRoute( "admin/courses/add", "GET", "AdminController", "prepareAddCourse" );
-$r->addRoute( "admin/courses/add", "POST", "AdminController", "addCourse" );
-
-$r->addRoute( "admin/courses/","GET","RedirectController","goAdmin");
-$r->addRoute( "admin/subjects/","GET","RedirectController","goAdmin");
-
-// TODO learnit.com/admin/admin/courses/edit-delete 
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/edit-delete
-$r->addRoute("admin/courses/edit-delete", "GET", "AdminController", "editDeleteCourses");
-
-// learnit.com/admin/courses/delete/:ID
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/delete/:ID
-$r->addRoute("admin/courses/delete/:ID", "GET", "AdminController", "deleteCourse");
-// learnit.com/admin/courses/edit/:ID
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/edit/:ID
-$r->addRoute("admin/courses/edit/:ID", "GET", "AdminController", "editCourse");
-$r->addRoute("admin/courses/edit/:ID", "POST", "AdminController", "updateCourse");
-
-
-
-// TODO learnit.com/admin/admin/subjects/add 
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/add
-$r->addRoute("admin/subjects/add", "GET", "AdminController", "prepareAddSubject" );
-$r->addRoute("admin/subjects/add", "POST", "AdminController", "addSubject" );
-
-// TODO learnit.com/admin/admin/subjects/edit-delete 
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/edit-delete
-$r->addRoute("admin/subjects/edit-delete", "GET", "AdminController", "editDeleteSubjects");
-
-// learnit.com/admin/subjects/delete/:ID
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/delete/:ID
-$r->addRoute("admin/subjects/delete/:ID", "GET", "AdminController", "TODO");
-// learnit.com/admin/subjects/edit/:ID
-// http: //localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/edit/:ID
-$r->addRoute("admin/subjects/edit/:ID", "GET", "AdminController", "TODO");
 
 
 $r->addRoute("login", "GET","PublicController", "login" );
@@ -79,6 +37,52 @@ $r->addRoute("logout", "GET","PublicController", "logout" );
 // Comentar ésta y descomentar la siguiente para generar hash manualmente
 $r->addRoute("sign-in-check", "POST","PublicController", "signInCheck" );
 // $r->addRoute("sign-in-check", "POST","PublicController", "getPassHash" );
+
+
+
+
+// learnit.com/admin
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin
+$r->addRoute( "admin", "GET", "AdminController", "admin" );
+
+// learnit.com/admin/courses/add
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/add
+$r->addRoute( "admin/courses/add", "GET", "AdminController", "prepareAddCourse" );
+$r->addRoute( "admin/courses/add", "POST", "AdminController", "addCourse" );
+
+$r->addRoute( "admin/courses/","GET","RedirectController","goAdmin");
+$r->addRoute( "admin/subjects/","GET","RedirectController","goAdmin");
+
+// TODO learnit.com/admin/admin/courses/edit-delete 
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/edit-delete
+$r->addRoute("admin/courses/edit-delete", "GET", "AdminController", "editDeleteCourses");
+
+// learnit.com/admin/courses/delete/:ID
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/delete/:ID
+$r->addRoute("admin/courses/delete/:ID", "GET", "AdminController", "deleteCourse");
+
+// learnit.com/admin/courses/edit/:ID
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/courses/edit/:ID
+$r->addRoute("admin/courses/edit/:ID", "GET", "AdminController", "editCourse");
+$r->addRoute("admin/courses/edit/:ID", "POST", "AdminController", "updateCourse");
+
+
+// TODO learnit.com/admin/admin/subjects/add 
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/add
+$r->addRoute("admin/subjects/add", "GET", "AdminController", "prepareAddSubject" );
+$r->addRoute("admin/subjects/add", "POST", "AdminController", "addSubject" );
+
+// TODO learnit.com/admin/admin/subjects/edit-delete 
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/edit-delete
+$r->addRoute("admin/subjects/edit-delete", "GET", "AdminController", "editDeleteSubjects");
+
+// learnit.com/admin/subjects/delete/:ID
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/delete/:ID
+$r->addRoute("admin/subjects/delete/:ID", "GET", "AdminController", "TODO");
+// learnit.com/admin/subjects/edit/:ID
+// http://localhost/web-2/TPE-WEB-2-%20Repo/admin/subjects/edit/:ID
+$r->addRoute("admin/subjects/edit/:ID", "GET", "AdminController", "TODO");
+
 
 
 // TODO Usar para 404
