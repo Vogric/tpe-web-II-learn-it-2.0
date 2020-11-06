85d41521d0ce31b7bc5d7b7d7f59baf97fa98445
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-11-06 16:49:32 -0300

    Base View creado
    * Refactorizamos la parte comun del manejo de sesión en las vistas.

--- View/AdminView.php
+++ View/AdminView.php
@@ -1,28 +1,9 @@
 <?php
-
+require_once "./View/BaseView.php";
 require_once "./libs/smarty/Smarty.class.php";
 
-class AdminView
+class AdminView extends BaseView
 {
-
-    private $smarty;
-
-    public function __construct()
-    {
-        $this->smarty = new Smarty();
-        $this->smarty->assign( 'base_url', BASE_URL );
-
-        if ( session_status() != PHP_SESSION_ACTIVE ) {
-            session_start();
-        }
-
-        if ( isset( $_SESSION['IS_LOGGED'] ) ) {
-            $this->smarty->assign( 'logged', true );
-            $this->smarty->assign( 'email_s', $_SESSION['EMAIL'] );
-        } else {
-            $this->smarty->assign( 'logged', false );
-        }
-    }
 
     public function showAdmin()
     {
--- View/UserView.php
+++ View/UserView.php
@@ -1,29 +1,9 @@
 <?php
-
+require_once "./View/BaseView.php";
 require_once "./libs/smarty/Smarty.class.php";
 
-class UserView
+class UserView extends BaseView
 {
-    private $smarty;
-
-    public function __construct()
-    {
-        $this->smarty = new Smarty();
-        $this->smarty->assign( 'base_url', BASE_URL );
-
-        if ( session_status() != PHP_SESSION_ACTIVE ) {
-            session_start();
-        }
-
-        if ( isset( $_SESSION['IS_LOGGED'] ) ) {
-            $this->smarty->assign( 'logged', true );
-            $this->smarty->assign( 'email_s', $_SESSION['EMAIL'] );
-        } else {
-            $this->smarty->assign( 'logged', false );
-        }
-
-    }
-
     public function showUser()
     {
         $this->smarty->assign( 'title_s', 'LearnIT - User' );
