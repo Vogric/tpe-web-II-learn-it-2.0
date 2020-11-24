86648d8c33fefa9f34fa66aa37688277afcd1daf
Aghu <andrefpgm@gmail.com>
2020-11-24 11:54:02 -0300

    API REST Comentarios WIP. No funcional
    * Mock in progress en APICommentsController
    * Creados View y Controller base para la API

--- api/APICommentsController.php
+++ api/APICommentsController.php
@@ -1,6 +1,8 @@
 <?php
+// require_once './Model/CommentModel.php';
+require_once './api/ApiController.php';
 
-class APICommentsController
+class APICommentsController extends APIController
 {
     public function getComments()
     {
