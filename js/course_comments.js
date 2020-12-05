34c184ea433fcfe6073efa61aff30f4cd8d5a84c
Aghu <andrefpgm@gmail.com>
2020-12-05 11:46:26 -0300

    Comments CSR WIP - comentarios traídos de la API
    * Los comentarios se fetchean de la API
    * El average ahora se calcula dinámicamente
    * Nota: El formatter de VSCodium cambió tambien algunas líneas

--- js/course_comments.js
+++ js/course_comments.js
@@ -3,41 +3,42 @@
 let app = new Vue({
     el: '#vue-course-comments',
     data: {
+        wip_message: null,
+        course_id: 0,
         comments: [],
-        wip_message: null
+        average: 0
     }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    app.wip_message="CSR WIP - INSERTED FROM VUE";
+    app.wip_message = "CSR WIP - INSERTED FROM VUE";
+
+    // Tomo el id directamente de la URL del curso que estoy visualizando 
+    // en el browser. eg: http://learnit.local/courses/2
+    // El último elemento es el id
+    app.course_id = window.location.href.split('/').pop();
+
+    let ENDPOINT = `api/course/${app.course_id}/comments`;
 
     getComments();
 
+
+    function getComments() {
+        fetch(ENDPOINT)
+            .then(response => response.json())
+            .then(function(comments) {
+                   // agrega stars según score a cada comentario 'c'
+                app.comments = comments.map(c => { c.stars = "⭐".repeat(c.score); return c });
+
+                let score_sum=0;
+                for (const c of comments) {
+                    score_sum += parseInt(c.score);
+                }
+                app.average = score_sum / comments.length;
+            })            
+            .catch(error => console.log(error))
+    }
+
+
+
 });
-
-
-function getComments() {
-    // TODO fetch de API los comments
-    let mocked_comments = [
-        {
-          username: "Good",
-          text: "Vue mocked 101: Lorem ipsum dolor sit amet, consectetur inim veniam, quis nostrud exercitation.",
-          score: 5
-        },
-        {
-            username: "Not bad",
-            text: "Vue mocked 102: Lorem ipsum dolor sit amet, consectetur adipiscing elit,liqua. Ut enim ad minim veniam, quis nostrud exercitation",
-            score: 3
-        },
-        {
-          username: "Bad course",
-          text: "Vue mocked 103: Lorem ipsum dolor sit amet, consectetur adipiscing elit,liqua. Ut enim ad minim veniam, quis nostrud exercitatio.",
-          score: 1
-        }
-    ];
-
-    // agrega stars según score
-    app.comments = mocked_comments.map(c => { c.stars="⭐".repeat(c.score); return c });
-
-    
-}
--- templates/vue/course_comments.vue
+++ templates/vue/course_comments.vue
@@ -1,45 +1,45 @@
 {literal}
 <div id="vue-course-comments">
 
-     <p>{{ wip_message }}</p>
-      
-      <div class="card-deck comment-form-box">
-        <form id="course-comment-form" class="card score md-form mb-3" action="#" method="POST">
-          <i class="fas fa-pencil-alt prefix"></i>
-          <textarea id="form22" class="md-textarea text-white form-control" rows="2"></textarea>
-          <label class="text-warning" for="form22">Leave a comment about this course</label>
-        </form>
+  <p>{{ wip_message }}</p>
+
+  <div class="card-deck comment-form-box">
+    <form id="course-comment-form" class="card score md-form mb-3" action="#" method="POST">
+      <i class="fas fa-pencil-alt prefix"></i>
+      <textarea id="form22" class="md-textarea text-white form-control" rows="2"></textarea>
+      <label class="text-warning" for="form22">Leave a comment about this course</label>
+    </form>
+  </div>
+
+  <!-- Promedio -->
+  <div v-if="comments.length > 0" class="card-deck average-box">
+    <div class="card score border-success mb-3">      
+      <div class="card-header average-score">Average</div>
+      <div class="card-body text-success">
+        <h5 class="card-title">{{ average }}</h5>
+        <p v-if="comment.length == 1" class="card-text">
+          Only 1 comment in total.</p>
+        <p v-if="comment.length > 1" class="card-text">
+          {{ comments.length }} comments in total.</p>
       </div>
+    </div>
+  </div> <!-- cierra Promedio -->
 
-      <!-- Promedio mock -->
-      <div class="card-deck average-box">        
-        <div class="card score border-success mb-3" >
-          <!-- TODO ajustar color de las letras en el card-header o
-                    rever como mostrar promedio -->
-          <div class="card-header average-score">Average</div>
-          <div class="card-body text-success">
-              <h5 class="card-title">3</h5>
-              <p class="card-text">3 comments in total.</p>
-          </div>
-        </div>
+
+  <!-- Un card deck contiene todos los comentarios -->
+  <div class="card-deck">
+    <!-- Un card para cada comentario -->
+
+    <div v-for="comment in comments" class="card score mb-3">
+      <div class="card-header">{{ comment.stars }}</div>
+      <div class="card-body text-success">
+        <h5 class="card-title">{{ comment.username }}</h5>
+        <p class="card-text">{{ comment.text }}</p>
       </div>
+    </div>
 
 
-      <!-- Un card deck contiene todos los comentarios -->
-      <div class="card-deck">
-        <!-- Un card para cada comentario -->
-
-        <div  v-for="comment in comments"
-              class="card score mb-3" >
-          <div class="card-header">{{ comment.stars }}</div>
-          <div class="card-body text-success">
-              <h5 class="card-title">{{ comment.username }}</h5>
-              <p class="card-text">{{ comment.text }}</p>
-          </div>
-        </div>
-
-
-      </div> <!-- cierra card-deck de comentarios -->
+  </div> <!-- cierra card-deck de comentarios -->
 
 
 </div><!-- cierra id="vue-course-comments" -->
